48cd19a58fa0661b4f088f42bb2144a79be6fa79
KelvinRyman <myouren.h@gmail.com>
2024-09-24 15:33:39 +0800

    Add variable initialization for 'b' in main function

--- main/main.cc
+++ main/main.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    int a = 0;
+    int a = 0, b = 0;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "a = " << a << std::endl;
     return 0;
