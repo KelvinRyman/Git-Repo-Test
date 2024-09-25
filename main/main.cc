ce64871021c985a1ef2a5af67cd43da5da583fb6
KelvinRyman <myouren.h@gmail.com>
2024-09-25 15:58:44 +0800

    Initialize and print variables 'a' and 'b' in main function

--- main/main.cc
+++ main/main.cc
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
     int a = 0, b = 0;
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "a = " << a << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
     return 0;
 }
 
